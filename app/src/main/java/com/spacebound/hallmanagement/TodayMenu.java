5f1c74dc0e39263a55b5563014e3bcef2c42c661
muhtasim50 <48941230+muhtasim50@users.noreply.github.com>
2019-12-24 21:38:27 +0600

    todaymenu.java edited

--- app/src/main/java/com/spacebound/hallmanagement/TodayMenu.java
+++ app/src/main/java/com/spacebound/hallmanagement/TodayMenu.java
@@ -1,14 +1,70 @@
-package com.spacebound.hallmanagement;
+package com.example.hallmanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class TodayMenu extends AppCompatActivity {
+public class TodayMenu extends AppCompatActivity implements View.OnClickListener {
+
+    private CardView breakfast,lunch,dinner,snacks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_today_menu);
+
+        breakfast = findViewById(R.id.breakfast);
+        lunch = findViewById(R.id.lunch);
+        dinner = findViewById(R.id.dinner);
+        snacks = findViewById(R.id.Snacks);
+
+        breakfast.setOnClickListener(this);
+        lunch.setOnClickListener(this);
+        dinner.setOnClickListener(this);
+        snacks.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        Intent intent;
+
+        switch (view.getId()){
+
+            case R.id.breakfast:
+            {
+                intent = new Intent(this,Breakfast.class);
+                startActivity(intent);
+                break;
+            }
+
+            case R.id.lunch:
+            {
+                intent = new Intent(this,Lunch.class);
+                startActivity(intent);
+                break;
+            }
+
+            case R.id.dinner:
+            {
+                intent = new Intent(this,Dinner.class);
+                startActivity(intent);
+                break;
+            }
+
+            case R.id.Snacks:
+            {
+                intent = new Intent(this,Snacks.class);
+                startActivity(intent);
+                break;
+            }
+
+        }
+
     }
 }
+
