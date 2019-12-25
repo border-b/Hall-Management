3ee569e9866a1a7668a53c2f3daf1f29d3de8a1a
muhtasim50 <48941230+muhtasim50@users.noreply.github.com>
2019-12-25 18:51:00 +0600

    Update StudentProfile.java

--- app/src/main/java/com/spacebound/hallmanagement/StudentProfile.java
+++ app/src/main/java/com/spacebound/hallmanagement/StudentProfile.java
@@ -1,22 +1,14 @@
 package com.spacebound.hallmanagement;
 
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.content.ClipData;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.Toolbar;
-
-import com.google.android.material.navigation.NavigationView;
 
 public class StudentProfile extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_profile);
+    }
 }
