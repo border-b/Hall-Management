9303ee531b3c6bf9fd7a8944accb0082899a576a
muhtasim50 <48941230+muhtasim50@users.noreply.github.com>
2019-12-26 12:49:56 +0600

    Add files via upload

---
+++ SignUp.java
@@ -0,0 +1,164 @@
+package com.spacebound.hallmanagement;
+
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class SignUp extends AppCompatActivity implements View.OnClickListener {
+
+    ProgressBar progressBar;
+    EditText editTextEmail, editTextPassword, editTextConPass, editTextRegNo;
+    EditText editTextDept, editTextPhone, editTextName;
+    DatabaseReference databaseReference;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("StudentUser");
+
+        editTextName = (EditText) findViewById(R.id.editTextName);
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        editTextConPass = (EditText) findViewById(R.id.editTextConPass);
+        editTextRegNo = (EditText) findViewById(R.id.editTextRegNo);
+        editTextPhone = (EditText) findViewById(R.id.editTextPhone);
+        editTextDept = (EditText) findViewById(R.id.editTextDept);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressbar);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        findViewById(R.id.buttonSignUp).setOnClickListener(this);
+        findViewById(R.id.textViewLogin).setOnClickListener(this);
+    }
+
+    private void registerUser() {
+        final String name = editTextName.getText().toString().trim();
+        final String regNo = editTextRegNo.getText().toString().trim();
+        final String email = editTextEmail.getText().toString().trim();
+        final String password = editTextPassword.getText().toString().trim();
+        final String confirmpass = editTextConPass.getText().toString().trim();
+        final String dept = editTextDept.getText().toString().trim();
+        final String phone = editTextPhone.getText().toString().trim();
+
+        if (name.isEmpty()) {
+            editTextName.setError("Name is required");
+            editTextName.requestFocus();
+            return;
+        }
+
+        if (regNo.length() != 10) {
+            editTextRegNo.setError("Please enter a valid Registration Number");
+            editTextRegNo.requestFocus();
+            return;
+        }
+
+        if (email.isEmpty()) {
+            editTextEmail.setError("Email is required");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextEmail.setError("Please enter a valid email");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (password.isEmpty()) {
+            editTextPassword.setError("Password is required");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if (password.length() < 6) {
+            editTextPassword.setError("Minimum lenght of password should be 6");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if (!(confirmpass.equals(password))) {
+            editTextConPass.setError("Passwords do not match");
+            editTextConPass.requestFocus();
+            return;
+        }
+
+        if (dept.isEmpty()) {
+            editTextDept.setError("Department Name cannot be empty");
+            editTextDept.requestFocus();
+            return;
+        }
+
+        if (phone.length() != 11) {
+            editTextPhone.setError("Please enter a valid phone number");
+            editTextPhone.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+
+
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressBar.setVisibility(View.GONE);
+                if (task.isSuccessful()) {
+
+                    String key = mAuth.getCurrentUser().getUid();
+                    StudentUser Student = new StudentUser(name, email, phone, regNo, dept);
+                    databaseReference.child(key).setValue(Student);
+                    finish();
+                    Toast.makeText(getApplicationContext(), "Registration Successful", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(SignUp.this, WelcomeStudent.class));
+                } else {
+
+                    if (task.getException() instanceof FirebaseAuthUserCollisionException) {
+                        Toast.makeText(getApplicationContext(), "You have already registered", Toast.LENGTH_SHORT).show();
+
+                    } else {
+                        Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.buttonSignUp:
+                registerUser();
+                break;
+
+            case R.id.textViewLogin:
+                finish();
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+        }
+    }
+}
