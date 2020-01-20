909bd1d4fdca8ae4de7e67a3ec1d0ee6a5b68461
Chandrashekhar Ghosh <42749862+chandrashekhar13@users.noreply.github.com>
2020-01-20 12:36:59 +0600

    Update Main2Activity.java

--- app/src/main/java/com/spacebound/hallmanagement/Main2Activity.java
+++ app/src/main/java/com/spacebound/hallmanagement/Main2Activity.java
@@ -1,28 +1,157 @@
 package com.spacebound.hallmanagement;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class Main2Activity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-    private Button adminlogin;
+public class Main2Activity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = "NewPostActivity";
+    private static final String REQUIRED = "Required";
+
+    //ProgressBar progressBar;
+    private DatabaseReference mDatabase;
+    EditText editTextEmail, editTextPassword;
+    FirebaseAuth mAuth;
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        adminlogin =(Button) findViewById(R.id.buttonLogin);
-        adminlogin.setOnClickListener(new View.OnClickListener() {
+
+        mAuth = FirebaseAuth.getInstance();
+
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        findViewById(R.id.buttonLogin).setOnClickListener(this);
+    }
+
+    private void userLogin() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if (email.isEmpty()) {
+            editTextEmail.setError("Email is required");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextEmail.setError("Please enter a valid email");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (password.isEmpty()) {
+            editTextPassword.setError("Password is required");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if (password.length() < 6) {
+            editTextPassword.setError("Minimum length of password should be 6");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+//        DatabaseReference userRef = FirebaseDatabase.getInstance().getReference().child("AdminList");
+//
+//        userRef.addValueEventListener(new ValueEventListener() {
+//
+//            boolean flag = false;
+//
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+//                    //User user = snapshot.getValue(User.class);
+//                    String email = snapshot.child("email").getValue(String.class);
+//                    if (email == null) continue;
+//                    //assert user != null;
+//                    if (email.equals(editTextEmail)) {
+//                        flag = true;
+//                        break;
+//                    }
+//                    //Log.println(Log.DEBUG, "", user.getId());
+//                }
+//                if (flag == false) {
+//                    Toast.makeText(getApplicationContext(), "no such admin exists", Toast.LENGTH_SHORT).show();
+//                    return;
+//                }
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//            }
+//        });
+
+
+
+//        userRef.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                String email = dataSnapshot.child("email").getValue().toString();
+//            }
+//
+//
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+//            }
+//        });
+//
+//        if (userRef == null) {
+//            Toast.makeText(getApplicationContext(), "no such admin exists", Toast.LENGTH_SHORT).show();
+//            return;
+//        }
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
-            public void onClick(View view) {
-                Intent intent15 = new Intent(Main2Activity.this, WelcomeAdmin.class);
-                startActivity(intent15);
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    finish();
+                    Intent intent = new Intent(Main2Activity.this, WelcomeAdmin.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.buttonLogin:
+                userLogin();
+                break;
+        }
     }
 }
